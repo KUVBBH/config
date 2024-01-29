b66d98235051d8bc3ab058eb4bbce3be694308b4
我是大菜鸡 <102891014+KUVBBH@users.noreply.github.com>
2024-01-29 13:42:28 +0800

    Add files via upload

--- .vim/mypy/HttpServer.py
+++ .vim/mypy/HttpServer.py
@@ -1,10 +1,12 @@
 from os import system
+from os.path import isfile, expanduser
 from sys import argv
 
-# system('nohup python -m http.server --directory /storage/emulated/0/Download  8000 > /dev/null 2>&1 &')
 
-if len(argv) == 1:
-    system("nohup python -m http.server 8000 > /dev/null 2>&1 &")
-    system('am start -a android.intent.action.VIEW -d "http://localhost:8000"')
+if not len(argv[1:]):
+    if isfile(f'{expanduser("~")}/.py_http_server.log'):
+        system("mv ~/.py_http_server.log ~/.last_py_http_server.log")
+    system("nohup python -m http.server 43960 > ~/.py_http_server.log 2>&1 &")
+    system('am start -a android.intent.action.VIEW -d "http://localhost:43960"')
 else:
     system('pgrep -f "python -m http.server" | xargs kill')
